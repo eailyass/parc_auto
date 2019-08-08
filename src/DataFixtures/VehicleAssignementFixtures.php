b467abdfbe2e3582a8b1a5f9ce40cd765ad13320
eailyass <eailyass@mydailyexperience.com>
2019-08-08 16:44:11 +0100

    add resources to api-platform

--- src/DataFixtures/VehicleAssignementFixtures.php
+++ src/DataFixtures/VehicleAssignementFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Driver;
+use App\Entity\Vehicle;
+use Faker;
+use App\Entity\VehicleAssignement;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +13,19 @@ class VehicleAssignementFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $vehicles = $manager->getRepository(Vehicle::class)->findAll();
+        $drivers = $manager->getRepository(Driver::class)->findAll();
+        $faker = Faker\Factory::create();
+
+        for ($i=0; $i <20 ; $i++) { 
+            $assignement[$i] = new VehicleAssignement();
+            $assignement[$i]->setDateOfRevoking($faker->dateTime())
+            ->setDateOfAssigning($faker->dateTime($max=$assignement[$i]->getDateOfRevoking()))
+            ->setVehicle($vehicles[rand(0,9)])
+            ->setDriver($drivers[rand(0,9)])
+            ->setObservation($faker->sentence());
+            $manager->persist($assignement[$i]);
+        }
 
         $manager->flush();
     }
